15d6098d912280a165123d012db37061a12e3c4c
Sjuul Janssen <sjuul@outlook.com>
2019-10-21 09:44:51 +0200

    first commit

---
+++ deploy.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+# for development purposed
+operator-sdk build kabisa/postgresdb-operator
+docker push kabisa/postgresdb-operator
+sleep 2
+export operator=`kubectl get pods | grep postgresdb-operator | awk '{print $1}'`
+kubectl delete pod $operator
